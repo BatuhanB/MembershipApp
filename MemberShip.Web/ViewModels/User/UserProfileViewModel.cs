68077e4350ebc9f33b0cfb80801ca869edf3f37e
batuhanb <balibatuhan19@gmail.com>
2023-11-22 16:11:20 +0300

    User Information page | change password added

---
+++ MemberShip.Web/ViewModels/User/UserProfileViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MemberShip.Web.ViewModels.User
+{
+    public class UserProfileViewModel
+    {
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public string? PhoneNumber { get; set; }
+    }
+}
