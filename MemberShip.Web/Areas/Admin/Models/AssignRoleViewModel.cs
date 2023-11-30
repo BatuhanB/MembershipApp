ff70d85b0a922ce74d6183f104a975b0ed1c6047
batuhanb <balibatuhan19@gmail.com>
2023-11-30 23:26:21 +0300

    Added assign role to user | show roles of user on the list

---
+++ MemberShip.Web/Areas/Admin/Models/AssignRoleViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MemberShip.Web.Areas.Admin.Models
+{
+    public class AssignRoleViewModel
+    {
+        public required string RoleId { get; set; }
+        public required string Name { get; set; }
+        public bool Exists { get; set; }
+    }
+}
